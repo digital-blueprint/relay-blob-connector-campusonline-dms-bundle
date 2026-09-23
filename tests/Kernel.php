566fdd7a7ba98f051af6553096fbb4c7075a89c0
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-23 13:11:34 +0200

    Port to CoreTestKernelTrait

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,59 +4,31 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BlobConnectorCampusonlineDmsBundle\Tests;
 
-use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Dbp\Relay\BlobBundle\DbpRelayBlobBundle;
 use Dbp\Relay\BlobConnectorCampusonlineDmsBundle\DbpRelayBlobConnectorCampusonlineDmsBundle;
 use Dbp\Relay\BlobLibrary\Api\BlobApi;
-use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
+use Dbp\Relay\CoreBundle\TestUtils\CoreTestKernelTrait;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Nelmio\CorsBundle\NelmioCorsBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+    use CoreTestKernelTrait;
 
     public const TEST_BUCKET_ID = 'document-bucket';
 
-    public function registerBundles(): iterable
+    protected function registerAdditionalBundles(): iterable
     {
-        yield new FrameworkBundle();
-        yield new SecurityBundle();
-        yield new TwigBundle();
-        yield new NelmioCorsBundle();
-        yield new MonologBundle();
         yield new DoctrineBundle();
         yield new DoctrineMigrationsBundle();
-        yield new ApiPlatformBundle();
         yield new DbpRelayBlobBundle();
         yield new DbpRelayBlobConnectorCampusonlineDmsBundle();
-        yield new DbpRelayCoreBundle();
     }
 
-    protected function configureRoutes(RoutingConfigurator $routes)
+    protected function configureAdditionalContainer(ContainerConfigurator $container): void
     {
-        $routes->import('@DbpRelayCoreBundle/Resources/config/routing.yaml');
-    }
-
-    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader)
-    {
-        $container->import('@DbpRelayCoreBundle/Resources/config/services_test.yaml');
-        $container->extension('framework', [
-            'test' => true,
-            'secret' => 'something',
-            'annotations' => false,
-        ]);
-
         $testConfig = [
             'authorization' => [
                 'roles' => [
